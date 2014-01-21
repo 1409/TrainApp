7cbe33e6e1932f0111051f2ed53b97ad3672e250
1409 <olli-91-@hotmail.de>
2014-01-21 15:33:42 +0100

    Validation-Regeln

--- app/models/trainingsplan.rb
+++ app/models/trainingsplan.rb
@@ -1,2 +1,6 @@
 class Trainingsplan < ActiveRecord::Base
+	validates :uebung, presence: true
+	validates :saetze, presence: true, numericality: true
+	validates :wiederholungen, presence: true, numericality: true
+	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-Trainingsplan.create(uebung: "Bankdrücken", saetze: "4", wiederholungen: "10", abgeschlossen: false)
+Trainingsplan.create(uebung: "Bankdrücken", saetze: 4, wiederholungen: 10, abgeschlossen: false)
+Trainingsplan.create(uebung: "Latziehen", saetze: 4, wiederholungen: 8, abgeschlossen: false)
+Trainingsplan.create(uebung: "Beinpresse", saetze: 4, wiederholungen: 8, abgeschlossen: false)
+Trainingsplan.create(uebung: "Arm-Curls", saetze: 4, wiederholungen: 8, abgeschlossen: false)
