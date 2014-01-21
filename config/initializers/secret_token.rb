278fc71d1f0bd87aa59c2919b7cc8042e4adb425
1409 <olli-91-@hotmail.de>
2014-01-21 15:14:10 +0100

    Kick-Off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrainApp::Application.config.secret_key_base = '8bb030bb9ffe912decfd14b87d76d077c92affcad514738d05abff642be45cc8bf3e57cfa10aa73cec5d3ab693ae250a6272d62a43fe04f8ad796639caf95cd4'
